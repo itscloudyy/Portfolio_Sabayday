8e320d8136f45783dcd16e544bf68cefbf8ca78b
unknown <sabaydayj@gmail.com>
2026-01-22 21:42:34 +0800

    new file added

--- app/app/components.js
+++ app/app/components.js
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <section className="hero">
-      <h1>Hi, I’m Johnwel 👋</h1>
+      <h1>Hi, I’m Johnwel Sabayday </h1>
       <p>
         I am a frontend-focused developer passionate about building clean,
         usable, and future-ready web applications.
